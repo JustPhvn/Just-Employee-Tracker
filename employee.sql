1a14d2175eec3f6736c4c5f451827615ff6c737c
JustPhvn <justicequyphan@gmail.com>
2020-03-08 20:17:24 -0700

    init commit, made employee db

---
+++ employee.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE employees_db;
+USE employees_db;
+
+CREATE TABLE employee(
+    id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY role_id REFERENCES role (id),
+    FOREIGN KEY manager_id REFERENCES employee (id)
+    );
+
+CREATE TABLE role(
+    id INT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY department_id REFERENCES department (id)
+    );
+
+CREATE TABLE department(
+    id INT,
+    dept_name VARCHAR(30),
+    PRIMARY KEY (id)
+    );
+
+
